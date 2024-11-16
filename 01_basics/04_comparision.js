552be43020201919101f1d26ed963e108b316f93
Vikram Singh <157872386+Vikram7737@users.noreply.github.com>
2024-11-16 17:36:36 +0000

    initial commit

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// < , > , == , === , !=;
+
+// with same data type 
+ 
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// with different data type
+
+// console.log("2" > 1);     // first convert string into number  then compare 
+// console.log( "02" > 1);   // same
+
+// console.log(null > 0);   // false   avoid this type of comparision
+// console.log(null == 0);  // false
+// console.log(null >= 0);  // true 
+
+// ===
+
+console.log("2" ===2 );   // data type also campared with ===
+
+
+
+
+
