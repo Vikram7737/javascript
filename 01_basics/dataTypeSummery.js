849c7af9c762aac5c427a6c7a338de06428e2ee4
Vikram Singh <157872386+Vikram7737@users.noreply.github.com>
2024-11-16 18:11:24 +0000

    initial commit

---
+++ 01_basics/dataTypeSummery.js
@@ -0,0 +1,36 @@
+// type of data types is based on , how data is stored in memory and access
+
+// two types 
+
+// 1- primitive // all the primitive data type are call by value
+
+// 7 type  : String , Number , boolean , null , undefined , Symbol => used for making uniqu , BigInt
+
+// const score = 100; // in js ther is no need of define type of data 
+
+// const id = Symbol('123')  // unique id
+// const anotherId = Symbol('123')  // unique id 
+// console.log(id === anotherId);  // false
+
+const bugNumber =346542136547899654n
+
+// js is dynamic typed language
+
+// 2- Reference (Non primitive)
+
+// Array , Objects, Functions 
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myobj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+
+
+
