8f0e2c47723a9cd20c4b19afe9ae53a47f4e22b4
Vikram Singh <157872386+Vikram7737@users.noreply.github.com>
2024-11-16 12:57:08 +0000

    inital commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version 
+ 
+// alert( 3 + 3) // we are using node js not browser
+
+console.log(3
+    +
+    3);  // code readibility should be high
+
+let name ="hitesh";
+let age = 15;
+let isLogedIn = false;
+let state = null;
+let gender;  // undefined
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null    => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
