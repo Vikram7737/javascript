5c001a89c9ab50f23c01a54bb9a1457b63a95a6c
Vikram Singh <157872386+Vikram7737@users.noreply.github.com>
2024-11-16 12:16:23 +0000

    initial commit

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const accountId = 144553;
+let accountEmail="hitesh@google.com";
+var accountPassword="1234";  
+/*
+prefer not to use var 
+bcs of issue in block scope and functional scope
+*/ 
+accountCity="jaipur";  // we can initialize variable in this way
+
+// accountId=2; not allowed
+
+accountEmail="vkpatel@gmail.com";
+accountPassword="12234";
+accountCity="hyderabad";
+
+
+
+
+
+console.table([accountId , accountEmail ,accountPassword , accountCity]);
