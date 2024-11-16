f00025c841f13eee858e4b56a81b23a94d9899ff
Vikram Singh <157872386+Vikram7737@users.noreply.github.com>
2024-11-16 17:20:51 +0000

    initial commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valuInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // gives NaN => not a number
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1;
+
+let isLoggedIn="hitesh"  // 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// " " => false
+// "hitesh" => true;
+
+
+// ************************** Operation *****************
+
+let value = 3 ;
+let negvalue = -value;
+console.log(negvalue);
+
+// console.log("1" + 2);     // 12
+// console.log(1 + "2");     // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+console.log(true) // true
+// console.log(+true) // 1  // don't write like this
+// console.log(+"")   // 0  // + used for type conversion 
+
+let gameCounter = 100 ;
+++gameCounter;              // pre increment 
+console.log(gameCounter);
+gameCounter++;             // post increment
+console.log(gameCounter);
+
+//   type conversion  
+
+
+
+
+
+
+
+ 
